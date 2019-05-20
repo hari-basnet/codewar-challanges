1c31cf927e77d7f69f30609f62b03e1fb0b4c8cf
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-05-20 23:57:03 +0300

    added fighter health challenge

--- codewars.js
+++ codewars.js
@@ -108,3 +108,18 @@ function findNextSquare(sq) {
 }
 
 console.log(findNextSquare(16))
+
+
+
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  var fac1 = Math.ceil( fighter1.health / fighter2.damagePerAttack );
+  var fac2 = Math.ceil( fighter2.health / fighter1.damagePerAttack );
+  if(fac1 < fac2) {
+    return fighter2.name;
+  } else if(fac2 < fac1) {
+    return fighter1.name;
+  } else {
+    return firstAttacker;
+  }
+}
